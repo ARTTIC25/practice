5e09d9b5f42d4f33e2f0fd4a5698b357dd6f12e7
ARTTIC25 <amithpanil252006@gmail.com>
2026-06-15 12:39:09 +0530

    simple UI

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pickle
+import streamlit as st
 
 movies=pickle.load(open("movies.pkl","rb"))
 similarity=pickle.load(open("similarity.pkl","rb"))
@@ -14,11 +15,20 @@ def recommend(movie):
 
   movie_list=sorted(distance,reverse=True,key=lambda x:x[1])[1:6]
   print("\nTop movie recommendation\n")
+  recommendation_list=[]
   for index,movie_data in enumerate(movie_list,start=1):
     movie_index=movie_data[0]
     similarity_score=round(movie_data[1]*100,2)
     title=movies.iloc[movie_index].title
-    print(f"{index}. {title} -> {similarity_score}% match")
+    #print(f"{index}. {title} -> {similarity_score}% match")
+    recommendation_list.append(title)
+  return recommendation_list
 
-movie=input("Enter the movie name :").lower()
-recommend(movie)
+st.title("Recommendation System\n")
+movie=st.text_input("Enter the movie name :")
+
+if st.button("recommend"):
+  recommendations=recommend(movie)
+
+for i in recommendations:
+  st.write(i)
